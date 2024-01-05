59daa853160e09de0c67220655ebe60f85757815
FredericoPAP <147717048+FredericoPAP@users.noreply.github.com>
2024-01-05 20:14:17 +0000

    Added LinkedIn, X and novaSBE logos and respective websites redirections. New popover button for Download. Correct logic when opening each element the others should dissappear.

--- FredaWebSite/js/app.js
+++ FredaWebSite/js/app.js
@@ -1,42 +1,75 @@
 document.addEventListener('DOMContentLoaded', function() {
   
-    // Get references to the button and the div
-    var teachingButton = document.getElementById('teachingButton');
-    var teachingExperienceDiv = document.getElementById('teachingExp');
-    var contentAtBottom = document.getElementById('contentAtBottom');
-    var contentAdded = false;
-    
-    // Add a click event listener to the button
-    teachingButton.addEventListener('click', function() {
-        if (!contentAdded) {
-        var newContent = document.createElement('div');
-        newContent.innerHTML = teachingExperienceDiv.innerHTML;
-        contentAtBottom.appendChild(newContent);
-        contentAdded = true;
-    } else {
-        // Hide content
-        contentAtBottom.innerHTML = ''; // Clear the content
-        contentAdded = false;
-    } 
-    });
-
 $(function () {
     $('#toggleAccordions').on('click', function(e) {
       $('.panel-collapse').collapse('toggle');
     })
   });
-  function showContent(contentId) {
-    // Hide all content elements
+  
+$("[data-toggle=popover]")
+  .popover({html:true})
+
+});
+
+var teachingButton = document.getElementById('teachingButton');
+var teachingExperienceDiv = document.getElementById('teachingExp');
+var contentAtBottom = document.getElementById('contentAtBottom');
+var contentAdded = false;
+
+// Teaching button click event
+teachingButton.addEventListener('click', function() {
+    if (!contentAdded) {
+    var newContent = document.createElement('div');
+    newContent.innerHTML = teachingExperienceDiv.innerHTML;
+    contentAtBottom.appendChild(newContent);
+    contentAdded = true;
+    hideDescription();
+    hideWorkContent();
+    hidePubContent();
+    hideWipContent();
+    
+} else {
+    // Hide Teaching content
+    contentAtBottom.innerHTML = '';
+    contentAdded = false;
+    hideWorkContent();
+    hidePubContent();
+    hideWipContent();
+} 
+});
+
+function showContent(contentId) {
     var contentElements = document.getElementsByClassName("content");
+    // Hide all elements from Research content
     for (var i = 0; i < contentElements.length; i++) {
         contentElements[i].style.display = "none";
     }
-
     // Show the selected content
     document.getElementById(contentId).style.display = "block";
+    hideDescription();
+    contentAtBottom.innerHTML = '';
+    contentAdded = false;
+    
+    
 }
+
 function hideDescription() {
     var descBody = document.getElementById('descBody');
     descBody.style.display = 'none';
 }
-});
+
+
+function hidePubContent() {
+    var workContent = document.getElementById('publicationsContent');
+    workContent.style.display = 'none';
+}
+
+function hideWorkContent() {
+    var workContent = document.getElementById('workContent');
+    workContent.style.display = 'none';
+}
+
+function hideWipContent() {
+    var workContent = document.getElementById('wipContent');
+    workContent.style.display = 'none';
+}

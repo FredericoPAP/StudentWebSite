8d0322aec1498d5a9c5decf60ac3127f091ca2a3
FredericoPAP <147717048+FredericoPAP@users.noreply.github.com>
2024-01-12 13:21:15 +0000

    Better UI/UX, improved CSS and readability in general; Full implementation of links and buttons;

--- FredaWebSite/js/app.js
+++ FredaWebSite/js/app.js
@@ -1,67 +1,133 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
-$(function () {
-    $('#toggleAccordions').on('click', function(e) {
-      $('.panel-collapse').collapse('toggle');
-    })
-  });
-  
+//CV download
 $("[data-toggle=popover]")
   .popover({html:true})
 
-  var teachingButton = document.getElementById('teachingButton');
-var teachingExperienceDiv = document.getElementById('teachingExp');
-var contentAtBottom = document.getElementById('contentAtBottom');
-var contentAdded = false;
+  var isPageRefreshed = sessionStorage.getItem('isPageRefreshed');
+    console.log(isPageRefreshed);
+    var descBody = document.getElementById("descBody");
+  if (isPageRefreshed) {
+      descBody.style.display = "block";
+      
+  } else{
+     descBody.style.display = "none";
+  }
+  sessionStorage.setItem('isPageRefreshed', true);
 
-console.log(teachingButton);
-console.log(teachingExperienceDiv);
-/*teachingButton.addEventListener('click', function () {
+    var teachingExperienceDiv = document.getElementById('teachingExp');
+    var dynamicContentContainer = document.createElement('div');
+    var workContent = document.getElementById('workContent');
+    var publicationsContent = document.getElementById('publicationsContent');
+    var wipContent = document.getElementById('wipContent');
+    var contentContainer = document.getElementById('contentContainer');
+    var newDiv = document.createElement('div');
+    var squareText = document.getElementById('squareText');
     
-    if (teachingExpDiv.style.display === 'none') {
-        teachingExpDiv.style.display = 'block';
-    } else {
-        teachingExpDiv.style.display = 'none';
+
+document.getElementById('teaching').addEventListener('click',function() {
+    while (contentContainer.firstChild) {
+        contentContainer.removeChild(contentContainer.firstChild);
+      }
+      dynamicContentContainer.innerHTML = '';
+    var contentDiv = document.createElement('div');
+    var hr = document.createElement('hr');
+    dynamicContentContainer.appendChild(hr);
+    contentDiv.innerHTML = teachingExperienceDiv.innerHTML;
+    dynamicContentContainer.appendChild(contentDiv);
+    squareText.appendChild(dynamicContentContainer);
+    isPageRefreshed = false
+    if(!isPageRefreshed){
+        document.getElementById("descBody").style.display = "none";
     }
-});*/
-
-// Teaching button click event
-teachingButton.addEventListener('click', function() {
-    if (!contentAdded) {
-    var newContent = document.createElement('div');
-    newContent.innerHTML = teachingExperienceDiv.innerHTML;
-    contentAtBottom.appendChild(newContent);
-    contentAdded = true;
-    hideDescription();
-    hideWorkContent();
-    hidePubContent();
-    hideWipContent();
-    
-} else {
-    // Hide Teaching content
-    contentAtBottom.innerHTML = '';
-    contentAdded = false;
-    hideWorkContent();
-    hidePubContent();
-    hideWipContent();
-} 
-}); 
-
 });
 
-
-function showContent(contentId) {
-    var contentElements = document.getElementsByClassName("content");
-    // Hide all elements from Research content
-    for (var i = 0; i < contentElements.length; i++) {
-        contentElements[i].style.display = "none";
+document.getElementById('research').addEventListener('click', function() {
+    while (dynamicContentContainer.firstChild) {
+        dynamicContentContainer.removeChild(dynamicContentContainer.firstChild);
     }
-    // Show the selected content
-    document.getElementById(contentId).style.display = "block";
-    hideDescription();
-    contentAtBottom.innerHTML = '';
-    contentAdded = false;
-};
+    while(contentContainer.firstChild) {
+    contentContainer.removeChild(contentContainer.firstChild);
+    }
+    newDiv.innerHTML = '';
+    clearDynamicContentContainer();
+    clearLinkDivs()
+    squareText.style.display = "none";
+    var workLink = document.createElement('a');
+    workLink.id = 'workContent';
+    workLink.href = '#work';
+    workLink.textContent = 'Work';
+    workLink.addEventListener('click', function() {
+        clearDynamicContentContainer();
+        var contentDiv = document.createElement('div');
+        var hr = document.createElement('hr');
+        dynamicContentContainer.appendChild(hr);
+        contentDiv.innerHTML = workContent.innerHTML;
+        dynamicContentContainer.appendChild(contentDiv);
+        squareText.appendChild(dynamicContentContainer);
+        squareText.style.display = 'block';
+        
+        
+    });
+    newDiv.appendChild(workLink);
+
+    var publicationsLink = document.createElement('a');
+    publicationsLink.id = 'publicationsContent';
+    publicationsLink.href = '#publications';
+    publicationsLink.textContent = 'Publications';
+    publicationsLink.addEventListener('click', function() {
+        clearDynamicContentContainer()
+        var contentDiv = document.createElement('div');
+        var hr = document.createElement('hr');
+        dynamicContentContainer.appendChild(hr);
+        contentDiv.innerHTML = publicationsContent.innerHTML;
+        contentDiv.style.marginBottom = '100px';
+        dynamicContentContainer.appendChild(contentDiv);
+        squareText.appendChild(dynamicContentContainer);
+        squareText.style.display = 'block';
+        console.log(dynamicContentContainer);
+    });
+    newDiv.appendChild(publicationsLink);
+
+    var wipLink = document.createElement('a');
+    wipLink.id = 'wipContent';
+    wipLink.href = '#wip';
+    wipLink.textContent = 'Work in Progress';
+    wipLink.addEventListener('click', function() {
+        clearDynamicContentContainer()
+        var contentDiv = document.createElement('div');
+        var hr = document.createElement('hr');
+        dynamicContentContainer.appendChild(hr);
+        contentDiv.innerHTML = wipContent.innerHTML;
+        dynamicContentContainer.appendChild(contentDiv);
+        squareText.appendChild(dynamicContentContainer);
+        squareText.style.display = 'block';
+        console.log(dynamicContentContainer);
+    });
+    clearDynamicContentContainer() 
+    newDiv.appendChild(wipLink)
+    contentContainer.appendChild(newDiv);
+    divWithLinksAdded = true;
+    document.getElementById('research').disabled= true;
+    console.log("DYNCONTENT CONTAINER" + dynamicContentContainer);
+    console.log("CONTENT CONTAINER" + contentContainer);
+   
+    isPageRefreshed = false;
+    if(!isPageRefreshed){
+        document.getElementById("descBody").style.display = "none";
+    }
+    console.log(isPageRefreshed);
+  });
+
+function clearLinkDivs() {
+    var contentDiv = document.createElement('hr');
+}
+function clearDynamicContentContainer() {
+    while (dynamicContentContainer.firstChild) {
+        dynamicContentContainer.removeChild(dynamicContentContainer.firstChild);
+    }
+}
+});
+
 
 function hideDescription() {
     var descBody = document.getElementById('descBody');
@@ -70,8 +136,8 @@ function hideDescription() {
 
 
 function hidePubContent() {
-    var workContent = document.getElementById('publicationsContent');
-    workContent.style.display = 'none';
+    var publicationsContent = document.getElementById('publicationsContent');
+    publicationsContent.style.display = 'none';
 };
 
 function hideWorkContent() {
@@ -80,6 +146,7 @@ function hideWorkContent() {
 };
 
 function hideWipContent() {
-    var workContent = document.getElementById('wipContent');
-    workContent.style.display = 'none';
+    var wipContent = document.getElementById('wipContent');
+    wipContent.style.display = 'none';
 };
+
